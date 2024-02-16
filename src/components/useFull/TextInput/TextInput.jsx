1fb5bcda08661a9e880aa43840e77e1e3a390007
rabbaniha <hrabbaniha@gmail.com>
2024-02-16 21:38:55 +0330

    create the custom input

---
+++ src/components/useFull/TextInput/TextInput.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import styles from "./styles.module.css"
+
+const TextInput = React.memo(({ placeholder, width, name, type, onChange, onBlur, value, error, touch }) => {
+    const [dirtyInput, setDirtyInput] = useState(false)
+    return (
+        <div
+            className={styles.container}
+            style={{
+                width: width,
+                borderColor: touch && error
+                    ? "var(--state-error-error)"
+                    : touch && !error
+                        ? "var(--state-success-success)"
+                        : "var(--neutral-white)"
+            }}
+        >
+            <input onInput={(e) => {
+                e.target.value === "" ? setDirtyInput(false) : setDirtyInput(true)
+            }} className={styles.input}
+                type={type}
+                name={name}
+                id={name}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+            />
+            <label htmlFor={name} className={!dirtyInput ? styles.placeholder : styles.dirty_placeholder}>{placeholder}</label>
+        </div>
+    )
+})
+
+export default TextInput
+
+
+
