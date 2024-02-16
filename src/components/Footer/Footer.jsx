02666c7dffe7d295daa429796fd499ef16a05b04
rabbaniha <hrabbaniha@gmail.com>
2024-02-16 21:39:32 +0330

    implement footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useFormik } from "formik"
+import * as Yup from "yup"
 import styles from "./styles.module.css"
 import { TbBrandTelegram } from "react-icons/tb";
 import { PiInstagramLogoThin } from "react-icons/pi";
 import { CiTwitter } from "react-icons/ci";
 import { Link } from 'react-router-dom';
+import TextInput from '../useFull/TextInput/TextInput';
+import Buttons from "../useFull/Buttons"
 
 
 const Footer = () => {
+  const persianWordRegex = /[پچجحخهعغفقثصضشسیبلاتنموئدذرزطظ]+/
+  const mobileRegex = /^09\d{9}$/
+  const validationSchema = Yup.object({
+    fullName: Yup.string()
+      .required("لطفا نام و نام خانوادگی خود را وارد نمائید.")
+      .matches(persianWordRegex, "لطفا صفحه کلید را به فارسی تغییر دهید و تنها از حروف استفاده نمائید.")
+      .min(7, "نام و نام خانوادگی نمی تواند از 7 حرف کمتر باشد."),
+    mobileNumber: Yup.string()
+      .required("لطفا شماره همراه خود را وارد نمائید.")
+      .matches(mobileRegex, "شماره همراه صحیح نیست."),
+    email: Yup.string()
+      .email("آدرس ایمیل صحیح نیست.")
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      mobileNumber: "",
+      email: ""
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+
+  
+
   return (
     <div className={styles.container}>
       <div className={styles.image_container}>
@@ -32,7 +65,59 @@ const Footer = () => {
             </div>
           </div>
           <div className={styles.left}>
-            <h4>پیام به ترخینه</h4>
+            <h5>پیام به ترخینه</h5>
+            <form className={styles.form} onSubmit={formik.handleSubmit}>
+              <div className={styles.right_inputs}>
+                <TextInput
+                  placeholder="نام و نام خانوادگی"
+                  width="267px"
+                  name="fullName"
+                  type={"text"}
+                  onChange={formik.handleChange}
+                  onBlue={formik.handleBlur}
+                  value={formik.values.fullName}
+                  error={formik.errors.fullName}
+                  touch={formik.dirty}
+                />
+
+                <TextInput
+                  placeholder="شماره تماس"
+                  width="267px"
+                  name="mobileNumber"
+                  type="phone"
+                  onChange={formik.handleChange}
+                  onBlue={formik.handleBlur}
+                  value={formik.values.mobileNumber}
+                  error={formik.errors.mobileNumber}
+                  touch={formik.dirty}
+
+                />
+                <TextInput
+                  placeholder="آدرس ایمیل"
+                  width="267px"
+                  name="email"
+                  type="email"
+                  onChange={formik.handleChange}
+                  onBlue={formik.handleBlur}
+                  value={formik.values.email}
+                  error={formik.errors.email}
+                  touch={formik.dirty}
+
+                />
+
+              </div>
+              <div className={styles.left_inputs}>
+                <textarea cols={37} rows={7} className={styles.textarea} placeholder='پیام شما' />
+                <Buttons
+                  text={"ارسال پیام"}
+                  bgColor={"transparent"}
+                  color={"var(--neutral-white)"}
+                  border={true}
+                  borderColor={"var(--neutral-white)"}
+                  exteraRadius={false}
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
