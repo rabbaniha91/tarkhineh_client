88fca14661f770244e3a4722eca728d46544e474
rabbaniha <hrabbaniha@gmail.com>
2024-02-20 12:30:04 +0330

    fixed search input

--- src/components/MenuFoods/MenuFoods.jsx
+++ src/components/MenuFoods/MenuFoods.jsx
@@ -2,26 +2,41 @@ import React, { useEffect, useState } from 'react'
 import styles from "./styles.module.css"
 import { IoIosArrowBack } from "react-icons/io";
 import FoodCart from '../useFull/FoodCart';
-
+import TextInput from "../useFull/TextInput/TextInput"
+import { CiSearch } from "react-icons/ci"
+import useScreenSize from '../../hooks/useScreenSize';
 
 const MenuFoods = ({ category, foolData }) => {
-
+    const { isXMD } = useScreenSize()
     useEffect(() => {
     }, [])
 
     return (
         <div className={styles.container}>
             <div className={styles.category}>
-                {category?.map((item, index) => (
-                    <a href={`#${item}`} key={item}>
-                        {item}
-                        <IoIosArrowBack size={20} color='var(--neutral-717171)' />
-                    </a>
-                ))}
+                <div>
+                    {category?.map((item, index) => (
+                        <a href={`#${item}`} key={item}>
+                            {item}
+                            <IoIosArrowBack size={20} color='var(--neutral-717171)' />
+                        </a>
+                    ))}
+
+                </div>
+                <TextInput
+                    placeholder="جستجو"
+                    width= {isXMD ? "90%" : "50%"}
+                    name="جستجو"
+                    type="text"
+                    borderColor="var(--neutral-black)"
+                    outlined={true}
+                    bgColor="var(--neutral-white)"
+                    icon={<CiSearch size={34} />}
+                />
             </div>
             <div className={""}>
                 {category?.map(item => (
-                    <>
+                    <div key={item}>
                         <h5 id={`${item}`} className={styles.title}>{item}</h5>
                         <div className={styles.food_container}>
                             {foolData.map(food => {
@@ -30,7 +45,7 @@ const MenuFoods = ({ category, foolData }) => {
                                 }
                             })}
                         </div>
-                    </>
+                    </div>
                 ))}
             </div>
         </div>
