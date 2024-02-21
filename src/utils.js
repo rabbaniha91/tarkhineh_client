b4db4aa58fe698d2558b1e5af138470b43082fcc
rabbaniha <hrabbaniha@gmail.com>
2024-02-21 10:32:25 +0330

    fixed some issues

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,16 @@ export const saveFoodsToLocalStorage = (food) => {
     let foods = []
     const prevItems = JSON.parse(localStorage.getItem("cartItems"));
     if (prevItems !== null) {
+        const existFood = prevItems.find(item => {
+            return item.foodName === food.foodName
+        })
+        console.log(existFood)
+        if (existFood !== undefined) return
+
         foods = [...prevItems, food]
     } else {
         foods.push(food)
     }
+
     localStorage.setItem("cartItems", JSON.stringify(foods))
 }
