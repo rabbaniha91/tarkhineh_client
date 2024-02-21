49af1aca73f092adf179d3a6e5b68e119f60a755
rabbaniha <hrabbaniha@gmail.com>
2024-02-21 10:00:24 +0330

    implement search in foods

--- src/components/MenuFoods/MenuFoods.jsx
+++ src/components/MenuFoods/MenuFoods.jsx
@@ -5,16 +5,34 @@ import FoodCart from '../useFull/FoodCart';
 import TextInput from "../useFull/TextInput/TextInput"
 import { CiSearch } from "react-icons/ci"
 import useScreenSize from '../../hooks/useScreenSize';
+import { Link } from 'react-router-dom';
+import { Divider } from 'antd';
+
+const handleSearch = (value, array) => {
+    const findFoodName = []
+    array.filter(food => {
+        if (food.foodName.includes(value)) {
+            findFoodName.push(food.foodName)
+        }
+
+    })
+
+    return findFoodName
+}
 
 const MenuFoods = ({ category, foolData }) => {
     const { isXMD } = useScreenSize()
+    const [showSearchBox, setShowSearchBox] = useState(false)
+    const [searchResult, setSearchResult] = useState(null)
     useEffect(() => {
     }, [])
+
+
 
     return (
         <div className={styles.container}>
             <div className={styles.category}>
-                <div>
+                <div className={styles.category_items}>
                     {category?.map((item, index) => (
                         <a href={`#${item}`} key={item}>
                             {item}
@@ -23,16 +41,37 @@ const MenuFoods = ({ category, foolData }) => {
                     ))}
 
                 </div>
-                <TextInput
-                    placeholder="جستجو"
-                    width= {isXMD ? "90%" : "50%"}
-                    name="جستجو"
-                    type="text"
-                    borderColor="var(--neutral-black)"
-                    outlined={true}
-                    bgColor="var(--neutral-white)"
-                    icon={<CiSearch size={34} />}
-                />
+                <div className={styles.search_box}>
+                    <TextInput
+                        placeholder="جستجو"
+                        width={isXMD ? "90%" : "100%"}
+                        name="جستجو"
+                        type="text"
+                        borderColor="var(--neutral-black)"
+                        outlined={true}
+                        bgColor="var(--neutral-white)"
+                        icon={<CiSearch size={34} />}
+                        onChange={(e) => {
+                            e.target.value !== "" ? setShowSearchBox(true) : setShowSearchBox(false)
+                            setSearchResult(handleSearch(e.target.value, foolData))
+                        }}
+                    />
+                    {showSearchBox && (
+                        <ul className={styles.search_result}>
+                            {searchResult !== null && searchResult.map(item => (
+
+
+                                <>
+                                    <li className={styles.search_items}>
+                                        <a href=''>{item}</a>
+                                    </li>
+                                    <Divider style={{margin: "0", padding: "0"}}/>
+                                </>
+
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
             <div className={""}>
                 {category?.map(item => (
