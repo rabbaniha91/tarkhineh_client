9aae3156e87ae7a62846ed4256755d962cafd448
rabbaniha <hrabbaniha@gmail.com>
2024-02-22 08:31:47 +0330

    set change state style for stepper

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./styles.module.css"
 import { BsInfoSquare } from "react-icons/bs";
 import { MdPayment } from "react-icons/md";
 import { IoCartOutline } from "react-icons/io5";
+import { FaRegCheckSquare } from "react-icons/fa";
 
 
-const Stepper = () => (
-    <div className={styles.container}>
+const Stepper = () => {
+    const [shoppingState, setShoppingState] = useState(1)
 
-        <span className={styles.steps_sign}>
-            <IoCartOutline className={styles.icons} />
-        </span>
-        <span className={styles.text}>سبد خرید</span>
-        <div className={styles.solid_line}></div>
-        <span className={styles.steps_sign}>
-            <BsInfoSquare className={styles.icons} />
-        </span>
-        <span className={styles.text}>تکمیل اطلاعات</span>
-        <div className={styles.solid_line}></div>
-        <span className={styles.steps_sign}>
-            <MdPayment className={styles.icons} />
-        </span>
-        <span className={styles.text}>پرداخت</span>
+    return (
+        <div className={styles.container}>
 
-    </div>
-);
+            <span className={`${styles.steps_sign} ${shoppingState >= 1 && styles.active_state}`}>
+                <IoCartOutline className={`${styles.icons} ${shoppingState >= 1 && styles.active_state}`} />
+            </span>
+            <span className={`${styles.text} ${shoppingState >= 1 && styles.active_state}`}>سبد خرید</span>
+            <div className={styles.solid_line}></div>
+            <span className={`${styles.steps_sign} ${shoppingState >= 2 && styles.active_state}`}>
+                <BsInfoSquare className={`${styles.icons} ${shoppingState >= 2 && styles.active_state}`} />
+            </span>
+            <span className={`${styles.text} ${shoppingState >= 2 && styles.active_state}`}>تکمیل اطلاعات</span>
+            <div className={styles.solid_line}></div>
+            <span className={`${styles.steps_sign} ${shoppingState === 3 && styles.active_state}`}>
+                <MdPayment className={`${styles.icons} ${shoppingState === 3 && styles.active_state}`} />
+            </span>
+            <span className={`${styles.text} ${shoppingState === 3 && styles.active_state}`}>پرداخت</span>
+
+        </div>
+    )
+};
 export default Stepper;
