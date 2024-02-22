43d01186fe2192c0c71d353fcf624e8cb01535be
rabbaniha <hrabbaniha@gmail.com>
2024-02-22 09:00:55 +0330

    implement cart item box based on empty cart

--- src/components/CartItemBox/CartItemBox.jsx
+++ src/components/CartItemBox/CartItemBox.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./styles.module.css"
+import { getCartItems } from "../../utils"
+import { Link } from 'react-router-dom'
 
 const CartItemBox = () => {
-  return (
-    <div className={styles.container}>CartItemBox</div>
-  )
+    const [cartItems, setCartItems] = useState(null)
+    useEffect(() => {
+        setCartItems(getCartItems())
+    }, [])
+
+
+    return (
+        <div
+            className={`
+        ${styles.container} 
+        ${cartItems === null && styles.empty_container}
+        `}>
+            {cartItems === null ? (
+                <>
+                    <span className={styles.text}>شما در حال حاضر هیچ سفارشی ثبت نکرده اید!</span>
+                    <Link to={"/menu"} className={styles.link_to_menu}>منوی رستوران</Link>
+                </>
+            ) : ""}
+        </div>
+    )
 }
 
 export default CartItemBox
