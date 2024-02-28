0f776d39dcc758a09b94a07a7c7710978bc6a6fc
rabbaniha <hrabbaniha@gmail.com>
2024-02-28 16:14:54 +0330

    responsive deliver method component

--- src/components/CompleteInformation/DeliveryMethods.jsx
+++ src/components/CompleteInformation/DeliveryMethods.jsx
@@ -4,20 +4,28 @@ import { HiOutlineTruck } from "react-icons/hi2";
 import { IoBasketOutline } from "react-icons/io5";
 
 import styles from "./styles.module.css"
+import useScreenSize from '../../hooks/useScreenSize';
+import { Divider } from 'antd';
 
 const DeliveryMethods = React.memo(({ method, setMethod }) => {
+    const { isSM } = useScreenSize()
     return (
         <div className={styles.delivery_method}>
             <div className={styles.title}>
                 <TbTruckDelivery size={34} />
                 <h4>روش تحویل سفارش</h4>
+
             </div>
+            {!isSM &&
+                <Divider style={{ margin: "0" }} />
+            }
+
             <div className={styles.methods}>
                 <input type="radio" name="delivery" id="delivery" />
                 <label style={{ display: "flex" }} htmlFor="delivery">
                     <div>
                         <h5>ارسال توسط پیک</h5>
-                        <span>توسط پیک رستوران ارسال شود.</span>
+
                     </div>
                     <HiOutlineTruck size={28} />
                 </label>
@@ -27,7 +35,9 @@ const DeliveryMethods = React.memo(({ method, setMethod }) => {
                 <label style={{ display: "flex" }} htmlFor="in_person">
                     <div>
                         <h5>تحویل حضوری</h5>
-                        <span>توسط پیک رستوران ارسال شود.</span>
+                        {isSM &&
+                            <span>توسط پیک رستوران ارسال شود.</span>
+                        }
                     </div>
                     <IoBasketOutline size={28} />
                 </label>
