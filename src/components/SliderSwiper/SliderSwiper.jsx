d5ad16ff3a22a7ee7a6cd6aff61d8876359847f9
rabbaniha <hrabbaniha@gmail.com>
2024-02-29 19:18:27 +0330

    implement custom arrows for swiper

--- src/components/SliderSwiper/SliderSwiper.jsx
+++ src/components/SliderSwiper/SliderSwiper.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import SecondFoodCart from '../useFull/SecondFoodCart'
+import styles from "./styles.module.css"
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-const SliderSwiper = ({ title }) => {
+const SliderSwiper = ({ title, foods, bgColor }) => {
+    const prevRef = useRef(null)
+    const nextRef = useRef(null)
+    const swiperRef = useRef(null)
+
+    useEffect(() => {
+        const prev = () => {
+            swiperRef.current?.swiper.slidePrev()
+        }
+        const next = () => {
+            swiperRef.current?.swiper.slideNext()
+        }
+        prevRef.current?.addEventListener("click", prev)
+        nextRef.current?.addEventListener("click", next)
+
+        return () => {
+            prevRef.current?.removeEventListener("click", prev)
+            nextRef.current?.removeEventListener("click", next)
+        }
+    }, [swiperRef.current, prevRef.current, nextRef.current])
+
     return (
-        <div>
-            <h4>{title}</h4>
+        <div style={{ backgroundColor: bgColor }} className={styles.conatiner}>
+            <div>
+                <h4>{title}</h4>
+                <swiper-container ref={swiperRef} className="mySwiper" slides-per-view="4"
+                    space-between="10" free-mode="true" >
+                    {foods?.map(food => (
+                        <swiper-slide>
+                            <SecondFoodCart food={food} />
+                        </swiper-slide>
+                    ))}
+
+                </swiper-container>
+                <span ref={prevRef} className={styles.prev}>
+                    <IoIosArrowForward color='var(--neutral-353535)' size={24} />
+                </span>
+                <span ref={nextRef} className={styles.next}>
+                    <IoIosArrowBack color='var(--neutral-353535)' size={24} />
+                </span>
+            </div>
         </div>
     )
 }
 
 export default SliderSwiper
+
+
--- src/pages/Branches.jsx
+++ src/pages/Branches.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Carousel from "../components/Carousel"
 import SliderSwiper from '../components/SliderSwiper'
+import { mainFoodData } from "../data/foodData"
 
 const Branches = () => {
+  const [specialOffer, setSpecialOffer] = useState(null)
+  useEffect(() => {
+    setSpecialOffer(mainFoodData.filter(food => {
+      return food.offer > 0
+    }))
+  }, [])
+
+
   return (
     <div>
-        <Navbar />
-        <Carousel />
-        <SliderSwiper title={"پیشنهاد ویژه"}/>
+      <Navbar />
+      <Carousel />
+      {specialOffer !== null && (
+        <SliderSwiper title={"پیشنهاد ویژه"} foods={specialOffer} bgColor={"var(--neutral-white)"}/>
+      )}
     </div>
   )
 }
